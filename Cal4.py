f6733d22b7fef2ca234bf462032fe8c7e9429f9b
Dev-SIMONONI <bankislimbo18@gmail.com>
2025-07-14 15:31:24 +0100

    setup for python config in config dirctorty

--- Cal4.py
+++ Cal4.py
@@ -1,6 +1,6 @@
 #Calculator V4
 Value1=input("enter a number \n >>>")
 Value2=input("enter a number \n >>>")
-Add= float(Value1) + float(Value2)
-print(Add)
+Sum= float(Value1) + float(Value2)
+print(f"Sum of {Value1} + {Value2} = {Sum}")
 
